87ee9b3deb1c3442f8fcb6a2421d2b7fa990976f
jennclee <8753352+jennclee@users.noreply.github.com>
2017-11-22 12:52:48 -0800

    Update searchEvents query, adds venue and category db tables

--- database/index.js
+++ database/index.js
@@ -15,21 +15,46 @@ knex.raw('DROP DATABASE IF EXISTS kickit;').then( () => {
   .then( () => {
     knex.raw(`DROP TABLE IF EXISTS events;`).then( (res) => {
       knex.schema.createTable('events', (table) => {
-        table.increments('id');
+        table.string('id').primary();
         table.string('name');
         table.text('description', 'longtext');
-        // table.foreign('venue_id');
-        table.string('price');
+        table.foreign('venue_id');
+        table.boolean('price');
         table.string('url');
         table.string('image_url');
         table.dateTime('start_datetime');
         table.dateTime('end_datetime');
-        // table.foreign('category_id');
+        table.foreign('category_id');
       })
       .then( (res) => {
         console.log('CREATE TABLE res: ', res);
       }).catch( ( err) => {
         console.log('Error occurred when creating events table: ', err);
+      })
+    }).then( () => {
+      knex.raw(`DROP TABLE IF EXISTS venues;`).then( (res) => {
+        knex.schema.createTable('venues', (table) => {
+          table.string('id').primary();
+          table.text('address', 'longtext');
+        })
+        .then( (res) => {
+          console.log('CREATE TABLE res: ', res);
+        }).catch( ( err) => {
+          console.log('Error occurred when creating venues table: ', err);
+        })
+      })
+    }).then( () => {
+      knex.raw(`DROP TABLE IF EXISTS categories;`).then( (res) => {
+        knex.schema.createTable('categories', (table) => {
+          table.string('id').primary();
+          table.string('shortname');
+          table.string('name');
+        })
+        .then( (res) => {
+          console.log('CREATE TABLE res: ', res);
+        }).catch( ( err) => {
+          console.log('Error occurred when creating categories table: ', err);
+        })
       })
     })
   })
@@ -40,6 +65,9 @@ const bookshelf = require('bookshelf')(knex);
 const _ = require('lodash');
 const Promise = require('bluebird');
 const moment = require('moment');
+
+const categoryList = require('../category_map.json');
+
 
 
 //==========================================================================================
@@ -52,8 +80,6 @@ class Event extends bookshelf.Model {
   }
 }
 
-
-
 const Events = bookshelf.Collection.extend({
   model: Event
 })
@@ -61,17 +87,12 @@ const Events = bookshelf.Collection.extend({
 
 module.exports = {
   // add events to table
+  // eventsList should be an array of event objects
+  // an event object should look like the following:
+    // {name: '', description: '', venue_id: '', price: '', url: '', image_url: '', start_datetime: '', end_datetime: '', category_id: '' }
   addEvents: (eventsList) => {
-    eventsList.forEach( (event) => {
-      Events.add(event);
-    });
-  },
-
-  // search for today's events
-  getTodaysEvents: () => {
-    Promise.resolve(Events.query( (qb) => {
-      qb.where('end_datetime', '>', moment().format()).orderBy('start_datetime', 'DESC');
-    }).fetch());
+    // {merge: true} updates existing models
+    Events.add(eventsList, [{merge: true}]);
   },
 
   // search for events in table
@@ -79,9 +100,23 @@ module.exports = {
   searchAllEvents: (date, categories, price) => {
     Promise.resolve(Events.query( (qb) => {
       qb.where('start_datetime', '=', date).andWhere('category_id', 'in', categories).andWhere('price', '=', price)
-    }).fetch());
+    }).fetchAll({
+      withRelated: [{
+        'venues': (qb) => {
+          qb.select('venues.id');
+          qb.columns('venues.name');
+        }
+      },
+      {
+        'categories': (qb) => {
+          qb.select('categories.id', 'categories.shortname');
+          qb.columns('categories.name');
+        }
+      }]
+    }));
   }
 }
+
 
 //==========================================================================================
 //                    Categories Table
@@ -97,36 +132,30 @@ const Categories = bookshelf.Collection.extend({
   model: Category
 })
 
-// add events to table
-// const addCategory = (categoryList) => {
-//   categoryList.forEach( (category) => {
-//     Categories.add(category)
-//   })
-// }
-
-// search for events in table
-const getCategoryName = (category_id) => {
-  return new Categories.query({where: {id: category_id}}).fetchOne()
+// add categories to table
+const addCategories = (categoryList) => {
+  Categories.add(categoryList.categories);
 }
+
+addCategories(categoryList);
+
 
 //==========================================================================================
 //                    Venues Table
 //==========================================================================================
 
-// create join events and categories table
-// let venues_Table = client.query((/* add query */));
+class Venue extends bookshelf.Model {
+  get tableName() {
+    return 'venues'
+  }
+}
 
-// // add to categories_events table 
-// let addToVenues_Events = client.query (/* add query */ (err, res) => {
-//   if (err) {
-//     return err;
-//   } else {
-//     return () => client.end();
-//   }
-// });
+const Venues = bookshelf.Collection.extend({
+  model: Venue
+})
 
-// //search categories_events table
-// let searchVenues_Events = client.query((/* add query */));
-
-
+// add categories to table
+const addVenue = (venue) => {
+  Venues.add(venue, [{merge: true}]);
+}
 
