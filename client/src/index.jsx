d9d5e58fe9777364e632e73efd87769bef7d7b66
Jon <jonathanclau8@gmail.com>
2017-11-22 15:55:25 -0800

    Formatting server index.js

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -33,6 +33,8 @@ class App extends React.Component {
 					today: events,
 				});
 			});
+		fetch('/loadVenues')
+			.then()
 	}
 
 	//create a function that is passed down to search Container
--- server/index.js
+++ server/index.js
@@ -5,13 +5,18 @@ const https = require("https");
 const getEvents = require('../lib/eventbrite.js');
 const Promise = require('bluebird');
 const PORT = process.env.PORT || 3000;
-const searchAllEvents = require('../database.index.js').searchAllEvents;
 
+const addEvents = require('../database.index.js').addEvents;
+const searchAllEvents = require('../database.index.js').searchAllEvents;
 
 const app = express();
 
 app.use(bodyParser.json());
 app.use(express.static(__dirname + '/../client/dist'));
+
+// ======================================================================
+//       Save month's data to the DB and send weekend Events to Client
+// ======================================================================
 
 app.get('/loadWeekend', function (req, res) {
   getEvents.month()
@@ -20,13 +25,14 @@ app.get('/loadWeekend', function (req, res) {
       let dataFormatted = parsed.events.map((event) => {
         let imageUrl = event.logo ? event.logo.url : 'https://cdn.evbstatic.com/s3-build/perm_001/f8c5fa/django/images/discovery/default_logos/4.png';    
         let catID = event.subcategory_id === 17001 ? event.subcategory_id : event.category_id; 
+        let defaultPrice = event.is_free? true : false;
         // is isFree is true, pass in true
         return {
           id: event.id,
           name: event.name.text,
           description: event.description.text,
           venue_id: event.venue_id,
-          price: event.is_free,
+          price: defaultPrice,
           url: event.url,
           image_url: imageUrl,
           start_datetime: event.start.local,
@@ -34,7 +40,7 @@ app.get('/loadWeekend', function (req, res) {
           cateogry_id: event.category_id,
         }
       });
-      // save dataFormatted to DB
+      addEvents(dataFormatted);
     
   getEvents.weekend()
     .then((data) =>{
@@ -43,22 +49,39 @@ app.get('/loadWeekend', function (req, res) {
   });
 });
 
+// ======================================================================
+//                    Query the DB on client filters
+// ======================================================================
 app.post('/filter', function(req,res) {
   let date = req.body.date;
   let categories = req.body.category;
   let price = req.body.price;
 
-  //searchAllEvents(date, categories, price);
-    
-})
+  searchAllEvents(date, categories, price);
+});
 
+// ======================================================================
+//                    Send today's data back to the client
+// ======================================================================
 app.get('/loadToday', function (req, res) {
   getEvents.today()
     .then((data) =>{
       res.json(data);
     });
 });
-    
+// ======================================================================
+//                    load Venues to DB
+// ======================================================================
+app.get('/loadVenues', function (req, res) {
+  getEvents.month()
+    .then((data) =>{
+      res.json(data);
+    });
+}); 
+
+// ======================================================================
+//                    Run Server
+// ======================================================================
 
 app.listen(PORT, function() {
   console.log(`listening on port ${PORT}!`);
