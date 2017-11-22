efe979d5a8f633461f4a5becf8986c5154dd1467
Jon <jonathanclau8@gmail.com>
2017-11-22 15:55:25 -0800

    Formatting server index.js

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -36,6 +36,8 @@ class App extends React.Component {
 				});
 				console.log('TODAYS events', this.state.today);
 			});
+		fetch('/loadVenues')
+			.then()
 	}
 
 	//create a function that is passed down to search Container
