726e2b8bd8729f0f201382f2aec88d6b4c94f06f
Jon <jonathanclau8@gmail.com>
2017-11-23 22:37:59 -0800

    Cleaned up comments

--- client/src/components/EventListContainer.jsx
+++ client/src/components/EventListContainer.jsx
@@ -46,20 +46,6 @@ class EventListContainer extends React.Component {
 		            inverted
 		          >
 		          {weekendRows}
-		          {
-		            // <Menu.Item name='home'>
-		            //   <Icon name='home' />
-		            //   Home
-		            // </Menu.Item>
-		            // <Menu.Item name='gamepad'>
-		            //   <Icon name='gamepad' />
-		            //   Games
-		            // </Menu.Item>
-		            // <Menu.Item name='camera'>
-		            //   <Icon name='camera' />
-		            //   Channels
-		            // </Menu.Item>
-		  	      }
 		          </Sidebar>
 		          <Sidebar.Pusher>
 		              <Segment basic className="row">
