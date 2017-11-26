fa2ce0c93231484410b4a0acdc57f7553391ecc6
Jon Lau <jonlau@Jons-MacBook-Air.local>
2017-11-26 11:51:36 -0800

    Search events rendering except imageURL

--- client/src/components/EventEntry.jsx
+++ client/src/components/EventEntry.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import categories from '../../../categories.json';
+import moment from 'moment';
 
 const EventEntry = (props) => {
 	let imageUrl = props.event.logo ? props.event.logo.url : 'https://cdn.evbstatic.com/s3-build/perm_001/f8c5fa/django/images/discovery/default_logos/4.png';
@@ -15,12 +16,12 @@ const EventEntry = (props) => {
 	return (
 		<div className="card">
 			<a href={props.event.url}>
-				<img src={imageUrl}/>
+				<img src={props.event.image_url||imageUrl}/>
 				<div className="card-body">
-					<time className="eventTime">{props.event.start_datetime}</time>
-					<div className="card-itle">{props.event.name.text}</div>
+					<time className="eventTime">{moment(props.event.start_datetime).format("MMM Do YY")}</time>
+					<div className="card-title">{props.event.name.text||props.event.name}</div>
 					<div className="eventLocation"></div>
-					<div className="event">{props.event.is_free ? 'Free' : 'FEE'}</div>
+					<div className="event">{props.event.is_free ? 'Free' : '$'}</div>
 					<div className="eventCategory">{getCategory(props.event.category_id)}</div>
 				</div>
 			</a>
@@ -29,3 +30,6 @@ const EventEntry = (props) => {
 };
 
 export default EventEntry;
+
+//https://img.evbuc.com/https%3A%2F%2Fcdn.evbuc.com%2Fimages%2F37366948%2F218111667493%2F1%2Foriginal.jpg$8h=200&w=450&rect=0%2C18%2C800%2C400&s=f2b505ed5825534f2694106e7ed37b2e
+//https://img.evbuc.com/https%3A%2F%2Fcdn.evbuc.com%2Fimages%2F36156815%2F181755422853%2F1%2Foriginal.jpg?h=200&amp;w=450&amp;rect=0%2C0%2C2160%2C1080&amp;s=de7dedd432b49bc9ca1b09b4f47349c7
